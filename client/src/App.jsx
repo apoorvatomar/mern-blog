1624d4884439d58e7217ec1e9ac79fc0f4a23c3c
Apoorva Tomar <apoorva.t@launchxlabs.com>
2024-03-24 06:27:20 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Signin } from './pages/Signin';
+import { SignUp } from './pages/SignUp';
+import { Dashboard } from './pages/Dashboard';
+import { Projects } from './pages/Projects';
+import { About } from './pages/About';
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/about' element={<About />} />
+      <Route path='/sign-in' element={<Signin/>} />
+      <Route path='/sign-up' element={<SignUp/>} />
+      <Route path='/dashboard' element={<Dashboard/>} />
+      <Route path='/projects' element={<Projects/>} />
+
+    </Routes>
+    
+    </BrowserRouter>
   )
 }
